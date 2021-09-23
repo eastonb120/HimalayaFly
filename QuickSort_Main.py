4c7ba307a8c4501d2cc519ee38b7b0bb7ee6f1d2
eastonb120 <eastonb120@gmail.com>
2021-09-22 20:21:26 -0500

    Adding the main file to repo

---
+++ QuickSort_Main.py
@@ -0,0 +1,13 @@
+#Item ADT as per project requirements
+
+class item:
+    def __init__(self, itemNumber, quantity, binNumber, inStock, name, price):
+        self.itemNumber = itemNumber
+        self.quantity = quantity
+        self.binNumber = binNumber
+        self.inStock = inStock
+        self.name = price
+
+
+#Create a list
+#TODO Val, how do you want to construct list of items?
