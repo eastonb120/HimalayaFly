aaee31d239deeab35e2b98e25ea8683ba5636464
eastonb120 <eastonb120@gmail.com>
2021-09-24 20:04:01 -0500

    Got recursive calls to work. Added print statements to evaluate its working, and to see how it works. Having issues appending lists when coming back up function call stack.

--- QuickSort_Main.py
+++ QuickSort_Main.py
@@ -15,21 +15,79 @@ class item:
         self.name = name
         self.price = price
 
+    def getItemNumber(self):
+        return self.itemNumber
+
 
 #Data representation?
 #TODO This constructor loop will use default values to build out the list so we can start testing the algorithm
-binNumbers = (56, 13, 25, 49, 65, 3, 53, 82, 9, 40, 31, 78, 17, 61, 47)
+binNumbers = [56, 13, 25, 49, 65, 3, 53, 82, 9, 40, 31, 78, 17, 61, 47]
 bins = list()
 for distinctNumber in binNumbers:
     bins.append(item(distinctNumber))
 
-for item in bins:
-    print("Item Number: " + str(item.itemNumber))
-    print("Item in stock: " + str(item.inStock))
-    print("Bin Number: " + str(item.binNumber))
-    print("Quantity: " + str(item.quantity))
-    print("Item Name: " + str(item.name))
-    print("\n---------------------\n")
+# for item in bins:
+#     print("Item Number: " + str(item.itemNumber))
+#     print("Item in stock: " + str(item.inStock))
+#     print("Bin Number: " + str(item.binNumber))
+#     print("Quantity: " + str(item.quantity))
+#     print("Item Name: " + str(item.name))
+#     print("\n---------------------\n")
 
 
+
+# pivotItem = bins.pop(len(bins) - 1)
+# lesser = list()
+# greater = list()
+# for item in bins:
+#     if(item.getItemNumber() < pivotItem.getItemNumber()):
+#         lesser.append(item)
+#     else:
+#         greater.append(item)
+#
+# lesserString = ""
+# for item in lesser:
+#     lesserString = lesserString + str(item.getItemNumber()) + ", "
+# print(lesserString)
+
+
+def quickSort(itemList):
+
+    if len(itemList) == 1:
+        return list(itemList)
+    else:
+        print("Length before pivot pop: " + str(len(itemList)))
+        pivotItem = itemList.pop(len(itemList) - 1)
+        print("pivotItem post pop: " + str(vars(pivotItem)))
+        print("Pivot value: " + str(pivotItem.getItemNumber()))
+        lesser = list()
+        greater = list()
+        print("Length after pivot pop: " + str(len(itemList)))
+        for item in itemList:
+            if (item.getItemNumber() < pivotItem.getItemNumber()):
+                print("Lesser: " + str(item.getItemNumber()))
+                lesser.append(item)
+            else:
+                print("Greater: " + str(item.getItemNumber()))
+                greater.append(item)
+
+
+        print("pivotItem post sort: " + str(vars(pivotItem)))
+
+        print("Lesser List Length: " + str(len(lesser)))
+        print("Greater List Length: " + str(len(greater)))
+        returnListL = quickSort(lesser)
+        print("Length after return lesser: " + str(len(returnListL)))
+        returnListP = returnListL.append(pivotItem)
+        print("Length after return pivotItem: " + str(len(returnListP)))
+        returnList = returnListP.extend(quickSort(greater))
+        return returnList
+
+
+
+sortedList = ""
+for item in quickSort(bins):
+    sortedList = sortedList + ", " + str(item.getItemNumber())
+
+print(sortedList)
 
