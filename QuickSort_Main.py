54c8776311a524b6b37c588f276f5fcc79ddeacb
eastonb120 <eastonb120@gmail.com>
2021-09-29 20:50:29 -0500

    QuickSort Complete. Waiting on CSV parsing to call item constructor.

--- QuickSort_Main.py
+++ QuickSort_Main.py
@@ -5,7 +5,7 @@
 # Additionally, this is a generalization of items also. We are generalizing such that all of the unique items in the
 # bins are just an item. Granted we have a name attribute, but it's still just an item.
 
-#TODO remove the defualt args and actually
+#TODO remove the defualt args and construct in another form
 class item:
     def __init__(self, itemNumber, quantity = 20, binNumber = 16, inStock = True, name = 'item', price = 1.25):
         self.itemNumber = itemNumber
@@ -18,41 +18,42 @@ class item:
     def getItemNumber(self):
         return self.itemNumber
 
-
-#Data representation?
+#
+# Data representation?
 #TODO This constructor loop will use default values to build out the list so we can start testing the algorithm
 binNumbers = [56, 13, 25, 49, 65, 3, 53, 82, 9, 40, 31, 78, 17, 61, 47]
 bins = list()
 for distinctNumber in binNumbers:
     bins.append(item(distinctNumber))
 
+# This is our quicksort function. It uses recursion
 def quickSort(itemList):
 
-    #This is the base case, we can't sort a list that contains only one item so we'll send it back up the call stack
+    # This is the base case, we can't sort a list that contains only one item so we'll send it back up the call stack
     if len(itemList) == 1:
         return itemList
     else:
-        #This will remove the right most value from the list given to the algorithm
+        # This will remove the right most value from the list given to the algorithm
         pivotItem = itemList.pop(len(itemList) - 1)
-        #We construct two empty lists that will have items assigned to them based on value
+        # We construct two empty lists that will have items assigned to them based on value
         lesserThan = list()
         greaterThan = list()
-        #This is where the sorting occurs. If the item is less than the pivot item, it goes to lesserThan than list
+        # This is where the sorting occurs. If the item is less than the pivot item, it goes to lesserThan than list
         for item in itemList:
             if (item.getItemNumber() < pivotItem.getItemNumber()):
                 lesserThan.append(item)
-        #If an item is greaterThan the value it is added to it's respective list.
+        # If an item is greaterThan the value it is added to it's respective list.
             else:
                 greaterThan.append(item)
 
-        #Append [lesserThan values] + pivot value + [greaterThan values]
-        #This is a recursive call to sort the "lesserThan" list prior to assigning it to the returnlist
+        # Append [lesserThan values] + pivot value + [greaterThan values]
+        # This is a recursive call to sort the "lesserThan" list prior to assigning it to the returnlist
         returnList = quickSort(lesserThan)
-        #We append the pivot point to the lesserThan list
+        # We append the pivot point to the lesserThan list
         returnList.append(pivotItem)
-        #This is a recursive call to sort the "greaterThan" list and extend it to the returnList
+        # This is a recursive call to sort the "greaterThan" list and extend it to the returnList
         returnList.extend(quickSort(greaterThan))
-        #Here we return the sorted list back up the call stack
+        # Here we return the sorted list back up the call stack
         return returnList
 
 
