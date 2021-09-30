a1ea5e2c148c42f11e49a7c809d6a07e42372486
eastonb120 <eastonb120@gmail.com>
2021-09-29 21:14:05 -0500

    QuickSort Complete. Waiting on CSV parsing to call item constructor. Added getters for all attributes.

--- QuickSort_Main.py
+++ QuickSort_Main.py
@@ -1,11 +1,11 @@
-#Item ADT as per project requirements
+# Item ADT as per project requirements
 
 # This class is an abstraction of item contained in a bins. This is because we are stripping away features of
 # items in order to make a description of the item more manageable to meet our end goal in a reasonable amount of time.
 # Additionally, this is a generalization of items also. We are generalizing such that all of the unique items in the
 # bins are just an item. Granted we have a name attribute, but it's still just an item.
 
-#TODO remove the defualt args and construct in another form
+# TODO remove the default args and construct in another form
 class item:
     def __init__(self, itemNumber, quantity = 20, binNumber = 16, inStock = True, name = 'item', price = 1.25):
         self.itemNumber = itemNumber
@@ -17,16 +17,32 @@ class item:
 
     def getItemNumber(self):
         return self.itemNumber
+    def getQuantity(self):
+        return self.quantity
+    def getBinNumber(self):
+        return self.binNumber
+    def isInStock(self):
+        return self.inStock
+    def getName(self):
+        return self.name
+    def getPrice(self):
+        return self.price
 
 #
 # Data representation?
-#TODO This constructor loop will use default values to build out the list so we can start testing the algorithm
+# TODO This constructor loop will use default values to build out the list so we can start testing the algorithm
+
+
 binNumbers = [56, 13, 25, 49, 65, 3, 53, 82, 9, 40, 31, 78, 17, 61, 47]
+
+
 bins = list()
 for distinctNumber in binNumbers:
     bins.append(item(distinctNumber))
 
 # This is our quicksort function. It uses recursion
+
+
 def quickSort(itemList):
 
     # This is the base case, we can't sort a list that contains only one item so we'll send it back up the call stack
@@ -56,9 +72,8 @@ def quickSort(itemList):
         # Here we return the sorted list back up the call stack
         return returnList
 
-
-
 sortedList = ""
+
 for item in quickSort(bins):
     sortedList = sortedList + " " + str(item.getItemNumber())
 
