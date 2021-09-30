49fed4cd2d2a283e2961af1b3d2e3e1a01ceab1a
eastonb120 <eastonb120@gmail.com>
2021-09-29 20:35:02 -0500

    QuickSort Complete. Waiting on CSV parsing to call item constructor.

--- QuickSort_Main.py
+++ QuickSort_Main.py
@@ -1,7 +1,7 @@
 #Item ADT as per project requirements
 
 # This class is an abstraction of item contained in a bins. This is because we are stripping away features of
-# items in order to make a description of the item more managable to meet our end goal in a reasonable amount of time.
+# items in order to make a description of the item more manageable to meet our end goal in a reasonable amount of time.
 # Additionally, this is a generalization of items also. We are generalizing such that all of the unique items in the
 # bins are just an item. Granted we have a name attribute, but it's still just an item.
 
@@ -26,68 +26,40 @@ bins = list()
 for distinctNumber in binNumbers:
     bins.append(item(distinctNumber))
 
-# for item in bins:
-#     print("Item Number: " + str(item.itemNumber))
-#     print("Item in stock: " + str(item.inStock))
-#     print("Bin Number: " + str(item.binNumber))
-#     print("Quantity: " + str(item.quantity))
-#     print("Item Name: " + str(item.name))
-#     print("\n---------------------\n")
-
-
-
-# pivotItem = bins.pop(len(bins) - 1)
-# lesser = list()
-# greater = list()
-# for item in bins:
-#     if(item.getItemNumber() < pivotItem.getItemNumber()):
-#         lesser.append(item)
-#     else:
-#         greater.append(item)
-#
-# lesserString = ""
-# for item in lesser:
-#     lesserString = lesserString + str(item.getItemNumber()) + ", "
-# print(lesserString)
-
-
 def quickSort(itemList):
 
+    #This is the base case, we can't sort a list that contains only one item so we'll send it back up the call stack
     if len(itemList) == 1:
-        return list(itemList)
+        return itemList
     else:
-        print("Length before pivot pop: " + str(len(itemList)))
+        #This will remove the right most value from the list given to the algorithm
         pivotItem = itemList.pop(len(itemList) - 1)
-        print("pivotItem post pop: " + str(vars(pivotItem)))
-        print("Pivot value: " + str(pivotItem.getItemNumber()))
-        lesser = list()
-        greater = list()
-        print("Length after pivot pop: " + str(len(itemList)))
+        #We construct two empty lists that will have items assigned to them based on value
+        lesserThan = list()
+        greaterThan = list()
+        #This is where the sorting occurs. If the item is less than the pivot item, it goes to lesserThan than list
         for item in itemList:
             if (item.getItemNumber() < pivotItem.getItemNumber()):
-                print("Lesser: " + str(item.getItemNumber()))
-                lesser.append(item)
+                lesserThan.append(item)
+        #If an item is greaterThan the value it is added to it's respective list.
             else:
-                print("Greater: " + str(item.getItemNumber()))
-                greater.append(item)
+                greaterThan.append(item)
 
-
-        print("pivotItem post sort: " + str(vars(pivotItem)))
-
-        print("Lesser List Length: " + str(len(lesser)))
-        print("Greater List Length: " + str(len(greater)))
-        returnListL = quickSort(lesser)
-        print("Length after return lesser: " + str(len(returnListL)))
-        returnListP = returnListL.append(pivotItem)
-        print("Length after return pivotItem: " + str(len(returnListP)))
-        returnList = returnListP.extend(quickSort(greater))
+        #Append [lesserThan values] + pivot value + [greaterThan values]
+        #This is a recursive call to sort the "lesserThan" list prior to assigning it to the returnlist
+        returnList = quickSort(lesserThan)
+        #We append the pivot point to the lesserThan list
+        returnList.append(pivotItem)
+        #This is a recursive call to sort the "greaterThan" list and extend it to the returnList
+        returnList.extend(quickSort(greaterThan))
+        #Here we return the sorted list back up the call stack
         return returnList
 
 
 
 sortedList = ""
 for item in quickSort(bins):
-    sortedList = sortedList + ", " + str(item.getItemNumber())
+    sortedList = sortedList + " " + str(item.getItemNumber())
 
 print(sortedList)
 
